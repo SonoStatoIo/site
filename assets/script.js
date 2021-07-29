7d33cd42628ddeaf1dd62fdae6dc781675d68222
SonoStatoIo <83108887+SonoStatoIo@users.noreply.github.com>
2021-07-29 22:21:18 +0200

    Delete assets directory

--- assets/script.js
+++
@@ -1,6 +0,0 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const link = document.getElementsByClassName("link")[0]
-
-toggleButton.addEventListener('click', () =>{
-    link.classList.toggle("active")
-})
