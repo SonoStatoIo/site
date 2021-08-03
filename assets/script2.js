ae479789fb4c6b14e4717dcf8b0191a755ce7c2e
SonoStatoIo <83108887+SonoStatoIo@users.noreply.github.com>
2021-08-03 12:39:10 +0200

    Add files via upload

--- assets/script2.js
+++ assets/script2.js
@@ -1,12 +1,6 @@
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const link = document.getElementsByClassName("link")[0]
-const copyright = document.getElementsByClassName("copyright")[0]
-
-var Year = new Date().getFullYear()
-
-copyright.innerHTML = "© " + Year + " by Daniele Petricone."
 
 toggleButton.addEventListener('click', () =>{
     link.classList.toggle("active")
 })
-
